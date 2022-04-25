21d39bfe590cd127cc24f5306ae45a069a3b8d5b
hritik01478 <hritikkumar01478@gmail.com>
2022-04-25 09:25:53 +0530

    objects added in the game

---
+++ script.js
@@ -0,0 +1,29 @@
+document.onkeydown = function (e) {
+    // console.log("The Key is: ", e.keyCode);
+    if (e.keyCode == 38) {
+        dino = document.querySelector('.dino');
+        dino.classList.add('animateDino');
+        setTimeout(() => {
+            dino.classList.remove('animateDino')
+        }, 700);
+    }
+}
+setInterval(() => {
+    dino = document.querySelector('.dino');
+    gameOver = document.querySelector('.gameOver');
+    obstacle = document.querySelector('.obstacle');
+
+    dx = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+    dy = parseInt(window.getComputedStyle(dino, null).getPropertyValue('top'));
+
+    ox = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('left'));
+    oy = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('top'));
+
+    offsetX = Math.abs(dx - ox);
+    offsetY = Math.abs(dy - oy);
+    console.log(offsetX, offsetY);
+    if (offsetX < 90 && offsetY < 50) {
+        gameOver.style.visibility = 'visible';
+        obstacle.classList.remove('obstacleAni');
+    }
+}, 100);
